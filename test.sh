afed386a0efef93f5460abf3f92e4c136babef7f
Zifan Xu <zifanxu96@gmail.com>
2022-04-05 21:00:06 -0500

    upload LfH

--- test.sh
+++ test.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 for i in {0..49} ; do
     n=`expr $i \* 6` # 50 test BARN worlds with equal spacing indices: [0, 6, 12, ..., 294]
-        for j in {1..10} ; do
-            # make sure all the ros processes are cleaned
-            killall -9 rosmaster
-            killall -9 gzserver
-            killall -9 gzclient
-            
+        for j in {1..10} ; do            
             # run the test
             python3 run.py --world_idx $n
             sleep 5
